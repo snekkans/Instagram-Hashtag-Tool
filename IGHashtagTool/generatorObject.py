b48ea2ed033b432d6dbca7a2591554c9faa0cb4e
Kevin Lopez <LopezKevin491@gmail.com>
2020-08-12 19:14:09 -0400

    nevermind we don't need a class

--- IGHashtagTool/generatorObject.py
+++
@@ -1,9 +0,0 @@
-class HashtagGenerator:
-    def __init__(self, min, max, last, recent, L, hashtag):
-        self.MIN_POSTS = min
-        self.MAX_POSTS = max
-        self.last = last
-        self.recent = recent
-        self.L = L
-        self.hashtag = hashtag
-
--- IGHashtagTool/main.py
+++ IGHashtagTool/main.py
@@ -6,7 +6,6 @@ from itertools import takewhile
 
 import instaloader
 import hashtagObject
-import generatorObject
 from collections import Counter
 from datetime import datetime, timedelta
 from instaloader import Hashtag
@@ -27,8 +26,6 @@ tempFiltered = []
 last = datetime.utcnow() - timedelta(days=30)
 recent = datetime.utcnow() - timedelta(days=2)
 
-gen = generatorObject.HashtagGenerator(MIN_POSTS,MAX_POSTS,last,recent,L,hashtag)
-print(gen.MIN_POSTS,gen.MAX_POSTS,gen.last,gen.recent,gen.L,gen.hashtag)
 posts = L.get_hashtag_posts(hashtag.name)
 print(hashtag.mediacount, "total posts for hashtag", hashtag.name, "\n")  # counts all posts in hashtag
 # for post in instaloader.Hashtag.from_name(L.context, 'legoleaks').get_posts():
