63065897fe89d32d97fc94d20a7e345bf11ee6d3
Kevin Lopez <LopezKevin491@gmail.com>
2020-08-25 21:26:43 -0400

    Update hashtagger.py
    no longer accepts input for timeframe, and instead gets data automatically for the last 30 days and the last 48 hours, goal here is to provide a clean path towards making it more configurable in the future

--- IGHashtagTool/hashtagger.py
+++ IGHashtagTool/hashtagger.py
@@ -18,17 +18,20 @@ L = instaloader.Instaloader()
 rawHashtagList = []
 masterHashtagList = []
 countedHashtags = []
-tempFiltered = []
+
+lastMonth = datetime.utcnow() - timedelta(days=30)
+last48 = datetime.utcnow() - timedelta(days=2)
+last24 = datetime.utcnow() - timedelta(days=1)
 
 
-def generate_hashtags(min_posts, max_posts, last, recent, hashtag, posts):
+def generate_hashtags(min_posts, max_posts, hashtag, posts):
+    global lastMonth, last48, last24
     print(hashtag.mediacount, "total posts for hashtag", hashtag.name, "\n")  # counts all posts in hashtag
-    # for post in instaloader.Hashtag.from_name(L.context, 'legoleaks').get_posts():
     print("Begin finding relevant posts\n")
     # get posts in timeframe
-    for post in takewhile(lambda p: p.date_utc > last, posts):
+    for post in takewhile(lambda p: p.date_utc > lastMonth, posts):
         # L.download_post(post, target='#' + hashtag.name)
-        print(post.date_utc, last, last < post.date_utc)
+        print(post.date_utc, lastMonth, lastMonth < post.date_utc)
         rawHashtagList.append(post.caption_hashtags)
     print("Finished finding relevant posts!\n")
     print("Begin generating hashtag list\n")
@@ -37,8 +40,10 @@ def generate_hashtags(min_posts, max_posts, last, recent, hashtag, posts):
         for j in i:
             masterHashtagList.append(j)
     # artopportunitiesmonthly
+
     c = Counter(masterHashtagList)
     hashtags = list(c)
+
     print("Finished generating hashtag list with", len(hashtags), "unique results\n")
     print(hashtags, "\n")
     print("Begin filtering hashtags by post count\n")
@@ -46,30 +51,29 @@ def generate_hashtags(min_posts, max_posts, last, recent, hashtag, posts):
         try:
             h = Hashtag.from_name(L.context, i)
             count = h.mediacount
+            new_tag = hashtagObject.HashtagObj(h.name, count, 0, 0, 0)
+            nposts = L.get_hashtag_posts(new_tag.getName())
             # if the count's not in the range
             if max_posts <= count or min_posts >= count:
                 print(i + " NOT in range", count)
             else:
                 print(i + " IS in range", count)
-                tempFiltered.append(h)
-                newTag = hashtagObject.HashtagObj(h.name, count, 0)
-                countedHashtags.append(newTag)
+                try:
+                    print("Begin getting posts from last 48 hours for", new_tag.getName())
+                    temp = 0
+                    for post in takewhile(lambda p: p.date_utc > last48, nposts):
+                        print(post.date_utc, last48, last48 < post.date_utc)
+                        temp += 1
+                    print("Found", temp, "posts for", new_tag.getName(), 'over 48 hours ')
+                    new_tag.setLast2DaysPosts(temp)
+                except:
+                    print("Error getting posts over last 48 hours for", new_tag.getName())
+
+                countedHashtags.append(new_tag)
         except:
             print("Can't find hashtag", i)
     print("\nFinished filtering hashtags by post count with", len(countedHashtags), "results\n")
-    for i in countedHashtags:
-        try:
-            print("Begin get posts for hashtag", i.getName() + "\n")
-            temp = 0
-            nposts = L.get_hashtag_posts(i.getName())
-            for post in takewhile(lambda p: p.date_utc > recent, nposts):
-                # L.download_post(post, target='#' + i.getName())
-                print(post.date_utc, recent, recent < post.date_utc)
-                temp += 1
-            print("Finished getting posts for hashtag", i.getName() + "\n")
-            i.setRecentPosts(temp)
-        except:
-            print("Can't find Counted Hashtag", i)
+
     for i in countedHashtags:
         print(i.getName(), i.getPostCount(), i.getRecentPosts())
 
@@ -78,27 +82,23 @@ def test():
     global countedHashtags
     min_posts = int(entry_min.get())
     max_posts = int(entry_max.get())
-    timeframe = int(entry_days.get())
-    last = datetime.utcnow() - timedelta(days=int(timeframe))
-    recent_timeframe = int(entry_recent_days.get())
-    recent = datetime.utcnow() - timedelta(days=int(recent_timeframe))
     hashtag = Hashtag.from_name(L.context, str(entry_hashtag.get()))
     posts = L.get_hashtag_posts(hashtag.name)
 
     # run code
-    generate_hashtags(min_posts, max_posts, last, recent, hashtag, posts)
+    generate_hashtags(min_posts, max_posts, hashtag, posts)
     popup_showinfo(countedHashtags)
 
 
 def popup_showinfo(items):
-    log = "Hashtag | Total Posts | Recent Posts\n"
+    log = "Hashtag | Total Posts | 48 Hours Posts\n"
     for i in items:
-        log += str(i.getName())+" | "+str(i.getPostCount())+" | "+str(i.getRecentPosts())+"\n"
+        log += str(i.getName()) + " | " + str(i.getPostCount()) + " | " + str(i.get2DaysPosts()) + "\n"
     showinfo("Hashtagger", log)
 
 
 root = Tk()
-root.geometry("320x360")
+root.geometry("320x240")
 frame = Frame(root)
 frame.pack()
 
@@ -115,20 +115,6 @@ label_max.pack()
 entry_max = Entry(frame, width=20)
 entry_max.insert(0, '100000')
 entry_max.pack(padx=5, pady=5)
-
-label_days = Label(frame, text="Days before today")
-label_days.pack()
-
-entry_days = Entry(frame, width=20)
-entry_days.insert(0, '30')
-entry_days.pack(padx=5, pady=5)
-
-label_recent_post_days = Label(frame, text="Days of Active Posts")
-label_recent_post_days.pack()
-
-entry_recent_days = Entry(frame, width=20)
-entry_recent_days.insert(0, '2')
-entry_recent_days.pack(padx=5, pady=5)
 
 label_hashtag = Label(frame, text="Hashtag to Use")
 label_hashtag.pack()
