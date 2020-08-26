ed1df6198529c511595ccce08eb2275d28dba6a8
Kevin Lopez <LopezKevin491@gmail.com>
2020-08-25 20:51:12 -0400

    Update hashtagObject.py

--- IGHashtagTool/hashtagObject.py
+++ IGHashtagTool/hashtagObject.py
@@ -1,8 +1,10 @@
 class HashtagObj:
-    def __init__(self, name, postCount,recentPosts):
+    def __init__(self, name, post_count, last_two_days_posts, last_day_posts, last_month_posts):
         self.name = name
-        self.postCount = postCount
-        self.recentPosts=recentPosts
+        self.postCount = post_count
+        self.last2DaysPosts = last_two_days_posts
+        self.lastDayPosts = last_day_posts
+        self.lastMonthPosts = last_month_posts
 
     def getName(self):
         return self.name
@@ -10,8 +12,20 @@ class HashtagObj:
     def getPostCount(self):
         return self.postCount
 
-    def getRecentPosts(self):
-        return self.recentPosts
+    def get2DaysPosts(self):
+        return self.last2DaysPosts
 
-    def setRecentPosts(self,newNum):
-        self.recentPosts=newNum
+    def setLast2DaysPosts(self, newNum):
+        self.last2DaysPosts = newNum
+
+    def getDayPosts(self):
+        return self.lastDayPosts
+
+    def setDayPosts(self, newNum):
+        self.lastDayPosts = newNum
+
+    def getMonthPosts(self):
+        return self.lastMonthPosts
+
+    def setMonthPosts(self, newNum):
+        self.lastMonthPosts = newNum
